d4a60b070c44337d3a2a83d5165c236d0e2c9478
cr1ystallll222 <artemidasixxx@gmail.com>
2026-01-30 22:29:04 +0300

    Initial commit

--- inspect_39.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const filePath = 'd:/Codes/4/112/nehuy/3/web/app/(landing)/page.tsx';
-const content = fs.readFileSync(filePath, 'utf8');
-const lines = content.split('\n');
-console.log('Line 39:', lines[38].substring(0, 200) + ' ... ' + lines[38].substring(lines[38].length - 200));
